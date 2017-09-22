7f7f972acc4c83deaacd293566d4d2ee1929a9fa
Junior Abranches <jr-abranches@hotmail.com>
2017-09-22 17:25:49 -0400

    Added pagination Articles/Tutorials welcomepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-		@posts = Post.all.limit(3).order("created_at desc")
+		@posts =    Post.all.order("created_at desc").paginate(page: params[:page], per_page: 3)
 		@projects = Project.all.limit(3).order("created_at desc")
 	end
 end
